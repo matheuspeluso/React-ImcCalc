c73d8a8db54cef340ed72655470fd9899079336f
Matheus Nascimento <matheuspeluso@gmail.com>
2023-10-12 15:47:17 -0300

    refactor: add the css of the ImcTable

--- src/components/ImcTable.jsx
+++ src/components/ImcTable.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import Button from "./Button.jsx"
+import "../components/ImcTable.css"
 
 const ImcTable = ({data}) => {
   return (
     <div id='result-container'>
-      <p id="imc-number">
-        Seu Imc: 
-      </p>
-      <p id="imc-info">
-        Situação atual:
-      </p>
+      <p id="imc-number">Seu Imc:</p>
+      <p id="imc-info">Situação atual:</p>
       <h3>Confira as classificações:</h3>
       <div id="imc-table">
-        <div id="table-header">
+        <div className="table-header">
           <h4>IMC</h4>
           <h4>Classificação</h4>
           <h4>Obesidade</h4>
+        </div>
           {data.map((item)=> (
             <div className="table-data" key={item.info}>
               <p>{item.classification}</p>
@@ -23,7 +21,6 @@ const ImcTable = ({data}) => {
               <p>{item.obesity}</p>
             </div>
           ))}
-        </div>
         <Button id={"back-btn"} text={"Voltar"}/>
       </div>
     </div>
